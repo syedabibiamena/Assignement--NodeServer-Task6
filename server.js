85bf0d06e524ba0b13cad6bfd58ba41e9aca1bad
Syeda Bibi Amena <syedabibiamena1@gmail.com>
2026-02-13 17:22:44 +0530

    updated server logic and css

--- server.js
+++ server.js
@@ -1,88 +1,70 @@
-import http from 'http';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import http from "http";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const my_file = fileURLToPath(import.meta.url);
-const my_dir = path.dirname(my_file);
+const myfileName = fileURLToPath(import.meta.url);
+const mydirName = path.mydirname(myfileName);
 
-const MY_PORT = 3000;
+const MYPORT = 3000;
 
-
-function to_Read_and_Send(res, my_filePath, content_Type, stsCode = 200) {
-  fs.readFile(my_filePath, (err, data) => {
+function serveFile(res, filePath, type = "text/html", statusCode = 200) {
+  fs.readFile(filePath, (err, data) => {
     if (err) {
-      console.log(err); 
-      res.writeHead(500, { 'Content-Type': 'text/html' });
-      res.end('<h1>Server Error (500)</h1>');
+      console.log(err);
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end("<h1>Server Error</h1>");
       return;
     }
 
-    res.writeHead(stsCode, { 'Content-Type': content_Type });
+    res.writeHead(statusCode, { "Content-Type": type });
     res.end(data);
   });
 }
 
 const my_server = http.createServer((req, res) => {
 
-  
-  if (req.url === '/favicon.ico') {
+  if (req.url === "/favicon.ico") {
     res.writeHead(204);
-    res.end();
-    return;
+    return res.end();
   }
 
-  let my_filePath = '';
-  let content_Type = '';
-  let stsCode = 200;
+  let myfilesPath = "";
+  let type = "text/html";
+  let status = 200;
 
   switch (req.url) {
 
-    case '/':
-    case '/home':
-      my_filePath = path.join(my_dir, 'pages', 'home.html');
-      content_Type = 'text/html';
+    case "/":
+    case "/home":
+      myfilesPath = path.join(mydirName, "pages/home.html");
       break;
 
-    case '/about':
-      my_filePath = path.join(my_dir, 'pages', 'about.html');
-      content_Type = 'text/html';
+    case "/about":
+      myfilesPath = path.join(mydirName, "pages/about.html");
       break;
 
-    case '/contact':
-      my_filePath = path.join(my_dir, 'pages', 'contact.html');
-      content_Type = 'text/html';
+    case "/contact":
+      myfilesPath = path.join(mydirName, "pages/contact.html");
       break;
 
-    case '/styles/style.css':
-      my_filePath = path.join(my_dir, 'styles', 'style.css');
-      content_Type = 'text/css';
+    case "/styles/style.css":
+      myfilesPath = path.join(mydirName, "styles/style.css");
+      type = "text/css";
       break;
 
     default:
-    
-      if (req.url.startsWith('/images/')) {
-
-        my_filePath = path.join(my_dir, req.url);
-
-        const extension = path.extname(my_filePath);
-
-        if (extension === '.png') content_Type = 'image/png';
-        else if (extension === '.jpg' || extension === '.jpeg') content_Type = 'image/jpeg';
-        else if (extension === '.svg') content_Type = 'image/svg+xml';
-        else content_Type = 'application/octet-stream';
-
+      if (req.url.startsWith("/images/")) {
+        myfilesPath = path.join(mydirName, req.url);
       } else {
-        
-        my_filePath = path.join(my_dir, 'pages', '404.html');
-        content_Type = 'text/html';
-        stsCode = 404;
+        myfilesPath = path.join(mydirName, "pages/404.html");
+        status = 404;
       }
   }
 
-  to_Read_and_Send(res, my_filePath, content_Type, stsCode);
+  serveFile(res, myfilesPath, type, status);
 });
 
-my_server.listen(MY_PORT, () => {
-  console.log(`Server is up and  running at http://localhost:${MY_PORT}`);
+my_server.listen(MYPORT, () => {
+  console.log("Server is up  and running at http://localhost:" + MYPORT);
 });
